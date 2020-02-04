1c3829115c7307a3f1176a7c8c89026e27cf8da2
Raphael Montani <raphael.muntani@gmail.com>
2020-02-04 14:06:31 -0300

    Check which classes are caller and callee

--- lib/trace2/class_listing.rb
+++ lib/trace2/class_listing.rb
@@ -1,16 +1,38 @@
 require 'forwardable'
 
+# Responsable for listing all accessed classes
 class ClassListing
   extend Forwardable
 
-  attr_reader :accessed_classes
+  attr_reader :accessed_classes, :callers
 
   def_delegators :@trace_point, :enable, :disable
 
   def initialize
     @accessed_classes = []
+    @callers = []
     @trace_point = TracePoint.new(:call) do |tp|
       @accessed_classes << tp.defined_class.to_s
+      # @callers << caller_locations
+      @callers << { class: tp.defined_class.to_s, method: tp.callee_id.to_s, caller: find_caller(caller, @callers), call_stack: caller }
     end
+  end
+  
+  private
+
+  def find_caller(call_stack, registered_callees)
+    caller_method = parse_caller_method(call_stack)
+    caller_class = registered_callees.find do |registered_callee|
+      registered_callee[:method] == caller_method
+    end
+    
+    caller_class.nil? ? nil : caller_class[:class]
+  end
+
+  def parse_caller_method(call_stack)
+    call_stack.map do |curr_call|
+      methods = curr_call.match(/\`(\S+)'$/)
+      methods[1] if !methods.nil?
+    end[1]
   end
 end
--- spec/trace2/class_listing_spec.rb
+++ spec/trace2/class_listing_spec.rb
@@ -1,52 +1,111 @@
 require 'spec_helper'
 
 describe ClassListing do 
-  context 'for a simple class' do
-    let(:simple_class) {
-      class Simple
-        def call_me; end
-      end
-      Simple.new
-    }
+  class Simple
+    def simple_call; end
+  end
+
+  class Nested
+    def initialize
+      @simple = Simple.new
+    end
+
+    def nested_call
+      @simple.simple_call
+    end
+
+    def nested_simple_call; end
+  end
+
+  class ComplexNesting
+    def initialize
+      @simple = Simple.new
+      @nested = Nested.new
+    end
+
+    def complex_call
+      @simple.simple_call
+      @nested.nested_call
+      complex_simple_call
+      @nested.nested_simple_call
+    end
     
-    it 'lists all acessed classes' do
-     class_listing = ClassListing.new
-     
-     class_listing.enable
-     simple_class.call_me
-     class_listing.disable
-     
-     expect(class_listing.accessed_classes).to include 'Simple'
+    private
+
+    def complex_simple_call; end
+  end
+
+  describe '#accessed_classes' do 
+    context 'for a simple class' do
+      it 'lists all acessed classes' do
+        class_listing = ClassListing.new
+        simple_class = Simple.new
+        
+        class_listing.enable
+        simple_class.simple_call
+        class_listing.disable
+
+        expect(class_listing.accessed_classes).to include 'Simple'
+      end
+    end
+
+    context 'for a class called inside another class' do
+      it 'list all accessed classes' do
+        class_listing = ClassListing.new
+        nested_class_call = Nested.new
+        
+        class_listing.enable
+        nested_class_call.nested_call
+        class_listing.disable
+
+        expect(class_listing.accessed_classes).to include('Simple', 'Nested')
+      end
     end
   end
 
-  context 'for a class called inside another class' do
-    let(:nested_class_call) { 
-      class Simple
-        def call_me; end
+  describe '#callers' do
+    context 'for a class calling another class' do
+      it 'show that the callee was called by the caller' do
+        class_listing = ClassListing.new
+        nested_class_call = Nested.new
+
+        class_listing.enable
+        nested_class_call.nested_call
+        class_listing.disable
+
+        callers = class_listing.callers.find { |c| c[:class] == 'Simple' }
+        
+        expect(callers[:class]).to eq "Simple"
+        expect(callers[:caller]).to eq "Nested"
       end
+    end
 
-      class Nested
-        def initialize
-          @simple = Simple.new
-        end
+    context 'for multiple calls inside a class' do
+      it 'is able to record multiple calls to different classes' do
+        class_listing = ClassListing.new
+        complex_nesting = ComplexNesting.new
 
-        def call_nested
-          @simple.call_me
-        end
+        class_listing.enable
+        complex_nesting.complex_call
+        class_listing.disable
+
+        callers = class_listing.callers
+
+        simple_calls = callers.select { |c| c[:class] == 'Simple' } 
+        simple_callers = simple_calls.map { |c| c[:caller] }
+        nested_calls = callers.select { |c| c[:class] == 'Nested' }
+        nested_callers = nested_calls.map { |c| c[:caller] }
+        complex_calls = callers.select { |c| c[:class] == 'ComplexNesting' }
+        complex_callers = complex_calls.map { |c| c[:caller] }
+
+        expect(simple_calls.length).to eq 2 
+        expect(simple_callers).to include('Nested', 'ComplexNesting')
+                                           
+        expect(nested_calls.length).to eq 2
+        expect(nested_callers).to include('ComplexNesting')
+
+        expect(complex_calls.length).to eq 2
       end
-
-      Nested.new
-    }
-
-    it 'list all accessed classes' do
-     class_listing = ClassListing.new
-     
-     class_listing.enable
-     nested_class_call.call_nested
-     class_listing.disable
-     
-     expect(class_listing.accessed_classes).to include('Simple', 'Nested')
     end
   end
 end
