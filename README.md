579787e718685814924b6fd938b7be98ca2c2967
Raphael Montani <raphael.muntani@gmail.com>
2020-02-19 22:40:29 -0300

    Remove ClassListing from the project
    ClassListing used path and method name to determine which class
    called other classes. This logic is weak and may lead to wrong
    results. That's why it's obsolete and as a consequence, removed
    from the project.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ```ruby
 $LOAD_PATH << 'path/to/trace2'
-require 'class_listing'
+require 'class_lister'
 require 'class_use'
 require 'rspec/core'
 
-cl = ClassListing.new
+cl = ClassLister.new
 cl.enable
 RSpec::Core::Runner.invoke
 cl.disable
--- rspec_runner.rb
+++ rspec_runner.rb
@@ -2,11 +2,11 @@
 
 require 'rspec/core'
 require 'rspec'
-require_relative 'lib/trace2/class_listing'
+require_relative 'lib/trace2/class_lister'
 require_relative 'lib/trace2/class_use'
 
-class_listing = ClassListing.new
-class_listing.enable
+class_lister = ClassLister.new
+class_lister.enable
 RSpec::Core::Runner.run(['spec/'], $stderr, $stdout)
-class_listing.disable
-puts class_listing.accessed_classes.tally
+class_lister.disable
+puts class_lister.accessed_classes.tally
