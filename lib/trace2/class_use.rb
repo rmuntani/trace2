0f311029ad3af4691ac3437277c3c0f40d109d58
Raphael Montani <raphael.muntani@gmail.com>
2020-02-25 21:00:44 -0300

    Add a simpler way to visualize call stack of a class

--- lib/trace2/class_use.rb
+++ lib/trace2/class_use.rb
@@ -4,9 +4,21 @@
 class ClassUse
   attr_accessor :name, :method, :stack_level, :caller_class, :path
 
+  def initialize(
+    name: nil, method: nil,
+    caller_class: nil, stack_level: nil,
+    path: nil
+  )
+    @name = name
+    @method = method
+    @caller_class = caller_class
+    @stack_level = stack_level
+    @path = path
+  end
+
   def self.build(trace_point: nil, caller_class: nil, stack_level: nil)
     ClassUse.new(
-      name: ClassUse.class_name(trace_point),
+      name: class_name(trace_point),
       method: trace_point.callee_id.to_s,
       path: trace_point.path,
       stack_level: stack_level,
@@ -19,16 +31,15 @@ class ClassUse
 
     trace_point.self.to_s
   end
+  private_class_method :class_name
 
-  def initialize(
-    name: nil, method: nil,
-    caller_class: nil, stack_level: nil,
-    path: nil
-  )
-    @name = name
-    @method = method
-    @caller_class = caller_class
-    @stack_level = stack_level
-    @path = path
+  def callers_stack
+    curr_class = caller_class
+    callers_stack = []
+    until curr_class.nil?
+      callers_stack.push(curr_class)
+      curr_class = curr_class.caller_class
+    end
+    callers_stack
   end
 end
--- spec/trace2/class_use_spec.rb
+++ spec/trace2/class_use_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe ClassUse do
-  describe '#build' do
+  describe '.build' do
     it 'successfully' do
       trace_point = double(
         'TracePoint',
@@ -30,7 +30,7 @@ describe ClassUse do
       expect(class_use.path).to eq '/file/path'
     end
 
-    it 'for a block' do
+    it 'builds for a block' do
       trace_point = double(
         'TracePoint',
         defined_class: nil,
@@ -55,6 +55,42 @@ describe ClassUse do
       expect(class_use.stack_level).to eq 39
       expect(class_use.caller_class.name).to eq 'Caller'
       expect(class_use.path).to eq '/file/path'
+    end
+  end
+
+  describe '#callers_stack' do
+    it 'successfully' do
+      class_use = ClassUse.new(caller_class: nil)
+
+      expect(class_use.callers_stack).to eq []
+    end
+
+    it 'for a single caller' do
+      caller_class = ClassUse.new(
+        caller_class: nil, name: 'Simple', method: 'simple_call'
+      )
+      callee_class = ClassUse.new(caller_class: caller_class)
+
+      expect(callee_class.callers_stack).to eq [caller_class]
+    end
+
+    it 'for a long stack of callers' do
+      first_caller = ClassUse.new(
+        caller_class: nil, name: 'First', method: 'first_call'
+      )
+      second_caller = ClassUse.new(
+        caller_class: first_caller, name: 'Second', method: 'second_call'
+      )
+      third_caller = ClassUse.new(
+        caller_class: second_caller, name: 'Third', method: 'third_call'
+      )
+      callee = ClassUse.new(
+        caller_class: third_caller, name: 'Callee', method: 'call'
+      )
+
+      expect(callee.callers_stack).to eq [
+        third_caller, second_caller, first_caller
+      ]
     end
   end
 end
