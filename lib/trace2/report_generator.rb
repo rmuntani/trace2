fc74f9ca42119bfb455f105ebc4f084c86feefef
Raphael Montani <raphael.muntani@gmail.com>
2020-03-21 19:44:04 -0300

    Change templates handling
    With the inclusion of new relationship parsers, the templates should be
    moved to a folder to improve organisation. The report generator has
    adapted to handle the new folder organisation.

--- lib/trace2/report_generator.rb
+++ lib/trace2/report_generator.rb
@@ -4,7 +4,7 @@ module Trace2
   # Generates a report of the relationship between the
   # classes that were used
   class ReportGenerator
-    TEMPLATE_PATH = 'report_template.html'
+    TEMPLATES_FOLDER = 'templates'
 
     def initialize(classes_uses:, relationship_parser:, output_path:)
       @classes_uses = classes_uses
@@ -25,10 +25,15 @@ module Trace2
     end
 
     def make_report_text(parsed_relationships)
-      report_template = File.read(TEMPLATE_PATH)
+      report_template = File.read(template_path)
       report_template.gsub(
         '<%= parsed_relationships %>', parsed_relationships
       )
+    end
+
+    def template_path
+      parser_name = @relationship_parser.name.split(':').last.downcase
+      "#{TEMPLATES_FOLDER}/#{parser_name}_template.html"
     end
 
     def create_report_file(report_text)
--- spec/trace2/report_generator_spec.rb
+++ spec/trace2/report_generator_spec.rb
@@ -13,8 +13,9 @@ describe Trace2::ReportGenerator do
       classes_uses = [instance_double('Trace2::ClassUse')]
       parsed_relationship = [{ caller: 'Caller', callee: 'Callee' }]
       relationship_parser = class_double(
-        'Trace2::ClassesRelationshipParser',
-        parse: parsed_relationship.to_json
+        'Trace2::RelationshipParser::Mermaid',
+        parse: parsed_relationship.to_json,
+        name: 'Trace2::RelationshipParser::Mermaid'
       )
 
       report_generator = Trace2::ReportGenerator.new(
@@ -28,6 +29,24 @@ describe Trace2::ReportGenerator do
 
       expect(relationship_parser).to have_received(:parse).with(classes_uses)
       expect(report).to include(parsed_relationship.to_json)
+    end
+
+    it 'uses a template depending on parser name' do
+      allow(File).to receive(:read).and_return('')
+      relationship_parser = class_double(
+        'Trace2::RelationshipParser::D3',
+        parse: '[]',
+        name: 'Trace2::RelationshipParser:D3'
+      )
+      report_generator = Trace2::ReportGenerator.new(
+        relationship_parser: relationship_parser,
+        classes_uses: [],
+        output_path: output_file
+      )
+
+      report_generator.run
+
+      expect(File).to have_received(:read).with('templates/d3_template.html')
     end
   end
 end
