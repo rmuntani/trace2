01b56d7c192e44f76fafb58e54ffab8eeb917aed
Raphael Montani <raphael.muntani@gmail.com>
2020-04-11 15:43:54 -0300

    Create ruby headers and C file mocks
    Inside the spec/trace2/ext/ folder, some headers and C files were
    created to mock Ruby functions. This allows testing C code without
    the overhead of using the entire Ruby implementation. It also allows for
    more control of Ruby functions, making testing easier.

---
+++ spec/trace2/ext/ruby.c
@@ -0,0 +1,81 @@
+typedef unsigned long rb_event_flag_t;
+
+typedef struct {
+  int num;
+  char* str;
+  void* tracearg;
+  int type;
+} VALUE;
+
+typedef struct {
+  VALUE path;
+  VALUE self;
+  VALUE callee_id; 
+  VALUE lineno;
+  rb_event_flag_t event_flag;
+} rb_trace_arg_t;
+
+rb_trace_arg_t *tracearg_mock;
+VALUE rb_cObject;
+
+VALUE rb_tracearg_self(rb_trace_arg_t* tracearg) { 
+  return tracearg->self;
+}
+
+VALUE rb_tracearg_path(rb_trace_arg_t* tracearg) { 
+  return tracearg->path;
+}
+
+char* rb_string_value_ptr(VALUE* str_klass) {
+  return str_klass->str;
+}
+
+VALUE rb_tracearg_callee_id(rb_trace_arg_t* tracearg) {
+  return tracearg->callee_id;
+}
+
+char* rb_id2name(VALUE id) { 
+  return id.str;
+}
+
+VALUE rb_tracearg_lineno(rb_trace_arg_t* tracearg) {
+  return tracearg->lineno;
+}
+
+int FIX2INT(VALUE integer) { 
+  return integer.num;
+}
+
+rb_trace_arg_t* rb_tracearg_from_tracepoint(VALUE tracepoint) {
+  return tracearg_mock;
+}
+
+rb_event_flag_t rb_tracearg_event_flag(rb_trace_arg_t* tracearg) { 
+  return tracearg->event_flag;
+}
+
+VALUE rb_define_class_under(VALUE class, char* name, VALUE super_class) { return class; }
+
+void rb_define_method(VALUE class, const char* name, void (*argv)(), int argc) { }
+
+int TYPE(VALUE obj) {
+  return obj.type; 
+}
+
+char* rb_class2name(VALUE obj) {
+  return obj.str;
+}
+
+char *rb_obj_classname(VALUE obj) {
+  return obj.str;
+}
+
+VALUE rb_str_new_cstr(const char* str) {
+  VALUE str_class;
+  str_class.str = str;
+  return str_class;
+}
+
+VALUE rb_define_module_under(VALUE module, const char* str) {
+  return module;
+}
---
+++ spec/trace2/ext/ruby.h
@@ -0,0 +1,51 @@
+#define RUBY_EVENT_CALL 0
+#define RUBY_EVENT_B_CALL 1
+#define RUBY_EVENT_RETURN 2
+#define RUBY_EVENT_B_RETURN 3
+
+#define T_MODULE 0
+#define T_CLASS 1
+
+typedef struct {
+
+} VALUE;
+
+typedef struct {
+
+} rb_trace_arg_t;
+
+VALUE rb_cObject;
+
+typedef unsigned long rb_event_flag_t;
+
+VALUE rb_tracearg_self(rb_trace_arg_t*);
+
+VALUE rb_tracearg_path(rb_trace_arg_t*);
+
+char* rb_string_value_ptr(VALUE*);
+
+VALUE rb_tracearg_callee_id(rb_trace_arg_t*);
+
+char* rb_id2name(VALUE);
+
+VALUE rb_tracearg_lineno(rb_trace_arg_t*);
+
+int FIX2INT(VALUE);
+
+rb_trace_arg_t* rb_tracearg_from_tracepoint(VALUE);
+
+rb_event_flag_t rb_tracearg_event_flag(rb_trace_arg_t*);
+
+VALUE rb_define_class_under(VALUE, char*, VALUE);
+
+void rb_define_method(VALUE, const char*, void (*)(), int);
+
+int TYPE(VALUE);
+
+char* rb_class2name(VALUE);
+
+char *rb_obj_classname(VALUE);
+
+VALUE rb_str_new_cstr(const char*);
+
+VALUE rb_define_module_under(VALUE, const char*);
